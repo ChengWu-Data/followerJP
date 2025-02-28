d394a5f16563eba8c6bf7513f2d2df7d6fa3d5ae
cheng-wu <datajourney.chengw@gmail.com>
2025-02-28 14:08:33 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -10,6 +9,11 @@ export default defineConfig({
         plugins: ['babel-plugin-styled-components'],
       },
     }),
-    svgr({ exportAsDefault: true }),
+    svgr({
+      svgrOptions: {
+        icon: true, // 启用 SVG 图标优化
+      },
+      exportAsDefault: true, // 默认导出 SVG
+    }),
   ],
 });

dee7a6fd321adb7f2476179ac2fa06c93e2cc295
ChengWu-Data <datajourney.chengw@gmail.com>
2025-02-28 13:32:55 -0600

    首次提交：添加插花项目文件

---
+++ src/data/projectData.js
@@ -0,0 +1,123 @@
+export const projectData = [
+  {
+    id: crypto.randomUUID(),
+    title: 'FF-Search',
+    description:
+      'A search application that utilizes the moogle API to search for Final Fantasy characters; users can search for the games and characters from the main series.',
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6+)', 'MVC', 'RESTfulAPI'],
+    imageUrl: '/rasters/FF-Search.webp',
+    mobileUrl: '/rasters/Mobile-FF-Search.webp',
+    repository: 'https://github.com/Mully7773/FF-Search',
+    livelink: 'https://mully7773.github.io/FF-Search/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Task-Manager',
+    description:
+      "A 'task manager' or todo application built with React. Users can add, delete, edit, and view their tasks. I've also included drag and drop functionality to reorder tasks, a mark as complete option, and a dark/light theme toggler.",
+    technologies: ['React', 'Styled-Components', 'Framer-Motion', 'Vite'],
+    imageUrl: 'rasters/Task-Manager.webp',
+    mobileUrl: 'rasters/Task-Manager.webp',
+    repository: 'https://github.com/Mully7773/Task-Manager',
+    livelink: 'https://task-manager-fm.netlify.app/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Rock, Paper, Scissors, Lizard, Spock!',
+    description:
+      "An application based on the traditional game with two extra player choices originating from 'The Big Bang Theory'. Users can play against a computer on their mobile device or desktop and try to get a high score.",
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6+)', 'MVC'],
+    imageUrl: 'rasters/RPSLS.webp',
+    mobileUrl: 'rasters/Mobile-RPSLS.webp',
+    repository: 'https://github.com/Mully7773/RPSLS',
+    livelink: 'https://mully7773.github.io/RPSLS/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Calculator',
+    description:
+      'A thematic and responsive calculator that allows users to easily add, subtract, and divide positive integers and floating point numbers.',
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6+)', 'MVC'],
+    imageUrl: 'rasters/Calculator.webp',
+    mobileUrl: 'rasters/Calculator.webp',
+    repository: 'https://github.com/Mully7773/Calculator',
+    livelink: 'https://mully7773.github.io/Calculator/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'FiberChart',
+    description:
+      'A web application I made with HTML, CSS, and JavaScript for a friend to help him connect fiber internet at work more efficiently.',
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6+)'],
+    imageUrl: 'rasters/FiberChart.webp',
+    mobileUrl: 'rasters/FiberChart.webp',
+    repository: 'https://github.com/Mully7773/FiberChart',
+    livelink: 'https://mully7773.github.io/FiberChart/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Huddle',
+    description:
+      "A mobile responsive landing page for a fictional messaging company called 'Huddle'.",
+    technologies: [
+      'React',
+      'Styled-Components',
+      'Vite',
+      'React-intersection-observer',
+    ],
+    imageUrl: 'rasters/Huddle.webp',
+    mobileUrl: 'rasters/Huddle.webp',
+    repository: 'https://github.com/Mully7773/Huddle',
+    livelink: 'https://huddle-nickm.netlify.app/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Advice Generator',
+    description:
+      'A mobile-responsive app that allows users to get random pieces of advice with the click of a button.',
+    technologies: [
+      'HTML',
+      'CSS',
+      'JavaScript (ES6+)',
+      'MVC',
+      'Advice Slip API (RESTful API)',
+    ],
+    imageUrl: 'rasters/Advice-Generator.webp',
+    mobileUrl: 'rasters/Advice-Generator.webp',
+    repository: 'https://github.com/Mully7773/Advice-Generator',
+    livelink: 'https://mully7773.github.io/Advice-Generator/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Etch-A-Glow',
+    description:
+      'This is a browser art pad inspired by the mechanical drawing toy, Etch A Sketch. I named it "Etch-A-Glow" because users can use glowing squares to sketch their designs.',
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6+)', 'html2pdf'],
+    imageUrl: 'rasters/Etch-A-Glow.webp',
+    mobileUrl: 'rasters/Etch-A-Glow.webp',
+    repository: 'https://github.com/Mully7773/Etch-A-Glow',
+    livelink: 'https://mully7773.github.io/Etch-A-Glow/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'AdventureMap',
+    description:
+      'This is an interactive map application I developed using HTML, CSS, and JavaScript. Users can record their adventures and details such as how much it cost, when it happened, and the duration for future reference.',
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6+)', 'Leaflet', 'Moment.js'],
+    imageUrl: '/rasters/Adventure-Map.webp',
+    mobileUrl: '/rasters/Adventure-Map.webp',
+    repository: 'https://github.com/Mully7773/AdventureMap',
+    livelink: 'https://mully7773.github.io/AdventureMap/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Nexter',
+    description:
+      "A fully responsive web page for a fictional luxury hotel company called 'Nexter'.",
+    technologies: ['React', 'Vite', 'SASS'],
+    imageUrl: 'rasters/Nexter.webp',
+    mobileUrl: 'rasters/Nexter.webp',
+    repository: 'https://github.com/Mully7773/Nexter',
+    livelink: 'https://nexter-nickm.netlify.app/',
+  },
+];

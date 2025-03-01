eb77eb841f27d9c8078fa3fc4859a5dcbb21b84a
cheng-wu <datajourney.chengw@gmail.com>
2025-03-01 14:38:45 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './i18n';
+// import './i18n';
 import { StyledLoadingContainer } from './styles/UI/StyledLoadingContainer';
 import SuspenseFallback from './styles/UI/SuspenseFallback';
 import { AppProvider } from './Context/Context';

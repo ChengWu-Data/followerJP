64e04027b4eb8772b4436c0d1b7e86d940347fd8
cheng-wu <datajourney.chengw@gmail.com>
2025-03-01 13:58:59 -0600

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,18 +6,14 @@ import {
 } from '../../styles/Projects/ProjectsLayout/StyledProjectsSection';
 import Project from './Project';
 import { useReducedMotion } from 'framer-motion';
-import { useTranslation } from 'react-i18next';
-import i18n from '../../i18n';
 
 const Projects = () => {
-  const { t } = useTranslation();
-  const lang = i18n.resolvedLanguage;
   const shouldReduceMotion = useReducedMotion();
 
   return (
     <StyledProjectsSection>
-      <StyledSectionHeading lang={lang}>
-        <h2 id="projects"> {t('projects-heading')}</h2>
+      <StyledSectionHeading>
+        <h2 id="projects">Projects</h2>
       </StyledSectionHeading>
       <StyledProjectsList>
         {projectData.map((project, index) => {

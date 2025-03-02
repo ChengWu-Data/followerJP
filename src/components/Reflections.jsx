5ff76e46d5580af14e7f2b3e1489a347392edc63
cheng-wu <datajourney.chengw@gmail.com>
2025-03-01 19:58:43 -0600

    Update Reflections.jsx

--- src/components/Reflections.jsx
+++ src/components/Reflections.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
-
-
+import { StyledSectionHeading } from '../../styles/UI/StyledSectionHeading.jsx';
 
 
 const ReflectionsContainer = styled.section`

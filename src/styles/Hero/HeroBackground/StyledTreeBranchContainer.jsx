90c9fda520e8282bb5d77747fcb62a8550eb41f2
cheng-wu <datajourney.chengw@gmail.com>
2025-03-01 19:04:33 -0600

    Update StyledTreeBranchContainer.jsx

--- src/styles/Hero/HeroBackground/StyledTreeBranchContainer.jsx
+++ src/styles/Hero/HeroBackground/StyledTreeBranchContainer.jsx
@@ -3,31 +3,15 @@ import { motion } from 'framer-motion';
 
 export const StyledTreeBranchContainer = styled(motion.div)`
   position: absolute;
-  top: -7rem; /* ✅ Moves tree up on desktop */
+  top: -7rem; /* ✅ Move tree up */
   left: -2rem;
   z-index: 100;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  @media ${props => props.theme.bp.desktopXS} {
-    top: -20rem;
-  }
-
-  & .tree-svg {
-    width: 52rem;
-    height: auto;
-
-    @media ${props => props.theme.bp.desktopL} {
-      width: 50rem;
-    }
-    @media ${props => props.theme.bp.desktopXS} {
-      width: 48rem;
-    }
-    @media ${props => props.theme.bp.mobileM} {
-      display: none !important; /* ✅ Only hide tree on mobile */
-    }
+  @media ${props => props.theme.bp.mobileM} {
+    display: none; /* ✅ Completely hide tree on mobile */
   }
 `;
-
 

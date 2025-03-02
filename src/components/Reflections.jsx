eae85290ad8c4d725a8927bbdd0e084e9522a453
cheng-wu <datajourney.chengw@gmail.com>
2025-03-01 19:45:30 -0600

    Update Reflections.jsx

--- src/components/Reflections.jsx
+++ src/components/Reflections.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import StyledSectionHeading from "../../styles/UI/StyledSectionHeading"; // Import StyledSectionHeading
+import StyledSectionHeading from "../../../styles/UI/StyledSectionHeading";
+
 
 const ReflectionsContainer = styled.section`
   max-width: 900px;
